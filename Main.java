bb373a92e756fd0d62b0670bcb8a33f8856285fe
sara <20233005742@estudantes.ifpr.edu.br>
2024-12-10 16:41:47 -0300

    main criado

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
+import sys.controller.MonstrosController;
+import sys.view.MonstrosView;
+
 public class Main {
     public static void main(String[] args) {
-        
+        MonstrosView view = new MonstrosView();
+        MonstrosController controller = new MonstrosController();
+        controller.iniciarMenu();
     }
 }
