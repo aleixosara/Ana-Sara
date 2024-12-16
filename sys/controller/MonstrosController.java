1adcf0dfabdc9a3d301cdfb885282d0cc0bce1a0
sara <20233005742@estudantes.ifpr.edu.br>
2024-12-16 14:04:10 -0300

    adicionamos o controller

--- sys/controller/MonstrosController.java
+++ sys/controller/MonstrosController.java
@@ -1,5 +1,30 @@
 package sys.controller;
 
+import sys.model.MonstrosModel;
+import sys.view.*;
+import sys.model.Monstros;
+
 public class MonstrosController {
+    private void cadastrarMonstro(){
+        monstros.add(monstro);
+        view.mostrarMensagem("Monstro cadastrado com sucesso!");
+
+    }
+
+    private void exibirMonstro(){
+        view.mostrarLista(monstros);
+    }
+
+    private void buscarMonstro(){
+        String nome = view.adicionarNome();
+        MonstrosModel monstroEncontrado = null;
+        for (MonstrosModel monstro : monstros) {
+            if(monstro.getNome().equalsIgnoreCase(nome)){
+                monstroEncontrado = monstro;
+                break;
+            }
+            
+        }
+    }
     
 }
